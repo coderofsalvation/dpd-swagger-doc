63615d4ff1d6232604c2fa5ba20b542b25771e90
Leon <info@leon.vankammen.eu>
2016-11-12 04:30:38 +0100

    removed dependency + bugfix

--- index.js
+++ index.js
@@ -1,72 +1,79 @@
-var jsdefault = require('json-schema-defaults')
-
-var addGet = function(pathname,method,swagger,endpoint,resourceConfig){
-	if( method == "get" && !endpoint.responses[200] ){
-		endpoint.responses[200] = {
-			type: "object", 
-			properties: resourceConfig.properties
-		}
-		if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
-		swagger.paths["/"+pathname+"/:id"]["get"] = endpoint
-		var endpoint = JSON.parse(JSON.stringify(endpoint))
-		endpoint.responses[200] = {
-			type: "array", 
-			items: [{
-					type:"object",
-					properties: resourceConfig.properties
-			}]
-		}
-		if( !swagger.paths["/"+pathname] ) swagger.paths["/"+pathname] = {}
-		swagger.paths["/"+pathname][method] = endpoint
-	}
-}
-
-var addPost = function(pathname,method,swagger,endpoint,resourceConfig){
-	if( method == "post" && !endpoint.parameters ){
-		if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
-		endpoint.parameters = [
-			{
-				"in": "body",
-				"name": "body",
-				"description": pathname+" object that needs to be added",
-				"required": true,
-				"schema": {
-					type: "object",
-          properties: resourceConfig.properties 
-				}
-			}
-		]
-		swagger.paths["/"+pathname][method] = endpoint
-	}
-}
-
-var addPut = function(pathname,method,swagger,endpoint,resourceConfig){
-	if( method == "put" && !endpoint.parameters ){
-		if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
-		endpoint.parameters = [
-			{
-				"in": "body",
-				"name": "body",
-				"description": pathname+" fields that needs to be updated",
-				"required": true,
-				"schema": {
-					type: "object",
-          properties: resourceConfig.properties 
-				}
-			}
-		]
-		swagger.paths["/"+pathname+"/:id"][method] = endpoint
-	}
-}
-
-var addDelete = function(pathname,method,swagger,endpoint,resourceConfig){
-	if( method == "delete" && !endpoint.parameters ){
-		if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
-		swagger.paths["/"+pathname+"/:id"][method] = endpoint
-	}
-}
-
 module.exports = function(){
+
+	var me = this
+
+	this.addGet = function(pathname,method,swagger,endpoint,resourceConfig){
+		if( method == "get" ){
+			if( !endpoint.responses[200] ){
+				endpoint.responses[200] = {
+					type: "object", 
+					properties: resourceConfig.properties
+				}
+			}
+			if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
+			swagger.paths["/"+pathname+"/:id"]["get"] = endpoint
+			var endpoint = JSON.parse(JSON.stringify(endpoint))
+			endpoint.responses[200] = {
+				type: "array", 
+				items: [{
+						type:"object",
+						properties: resourceConfig.properties
+				}]
+			}
+			if( !swagger.paths["/"+pathname] ) swagger.paths["/"+pathname] = {}
+			swagger.paths["/"+pathname][method] = endpoint
+		}
+	}
+
+	this.addPost = function(pathname,method,swagger,endpoint,resourceConfig){
+		if( method == "post" ){
+			if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
+			if( !endpoint.parameters ){
+				endpoint.parameters = [
+					{
+						"in": "body",
+						"name": "body",
+						"description": pathname+" object that needs to be added",
+						"required": true,
+						"schema": {
+							type: "object",
+							properties: resourceConfig.properties 
+						}
+					}
+				]
+			}
+			swagger.paths["/"+pathname][method] = endpoint
+		}
+	}
+
+	this.addPut = function(pathname,method,swagger,endpoint,resourceConfig){
+		if( method == "put" ){
+			if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
+			if( !endpoint.parameters ){
+				endpoint.parameters = [
+					{
+						"in": "body",
+						"name": "body",
+						"description": pathname+" fields that needs to be updated",
+						"required": true,
+						"schema": {
+							type: "object",
+							properties: resourceConfig.properties 
+						}
+					}
+				]
+			}
+			swagger.paths["/"+pathname+"/:id"][method] = endpoint
+		}
+	}
+
+	this.addDelete = function(pathname,method,swagger,endpoint,resourceConfig){
+		if( method == "delete" && !endpoint.parameters ){
+			if( !swagger.paths["/"+pathname+"/:id"] ) swagger.paths["/"+pathname+"/:id"] = {}
+			swagger.paths["/"+pathname+"/:id"][method] = endpoint
+		}
+	}
+
   this.initResources = function(swagger, dpd){
     for( var i in dpd ){
       if( i == "swagger" ) continue
@@ -85,10 +92,10 @@ module.exports = function(){
           var endpoint = sdef.methods[method]
           if( endpoint.public != undefined && !endpoint.public ) continue
           endpoint.responses = endpoint.responses || {}
-					addGet(i,method,swagger,endpoint,resourceConfig)
-					addPost(i,method,swagger,endpoint,resourceConfig)
-					addDelete(i,method,swagger,endpoint,resourceConfig)
-					addPut(i,method,swagger,endpoint,resourceConfig)
+					me.addGet(i,method,swagger,endpoint,resourceConfig)
+					me.addPost(i,method,swagger,endpoint,resourceConfig)
+					me.addDelete(i,method,swagger,endpoint,resourceConfig)
+					me.addPut(i,method,swagger,endpoint,resourceConfig)
         }
       }
     }
@@ -96,4 +103,4 @@ module.exports = function(){
   return this
 }
 
-module.exports = new module.exports()
+module.exports = module.exports()
