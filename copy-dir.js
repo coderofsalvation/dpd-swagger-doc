05206dd8a65428dd583909cc4d6cebbf05d701e2
Ezra Obiwale <contact@ezraobiwale.com>
2017-05-24 22:52:24 +0100

    Trying to automate copying of the resource directory

--- copy-dir.js
+++ copy-dir.js
@@ -1,3 +1,4 @@
-var path = require('path');
-mkdir('-p', 'resources/swagger');
-cp('-Rf', path.join(__dirname, '/resource/*'), 'resources/swagger');
+var path = require('path'),
+        fs = require('fs');
+fs.mkdir('-p', 'resources/swagger-test');
+fs.cp('-Rf', path.join(__dirname, '/resource/*'), 'resources/swagger-test');
